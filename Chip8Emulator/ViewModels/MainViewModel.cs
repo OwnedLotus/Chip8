46f12b4347ac4d7735807ad44ed7ef5116ebd3f0
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-11-28 16:14:25 -0500

    after commit

--- Chip8Emulator/ViewModels/MainViewModel.cs
+++ Chip8Emulator/ViewModels/MainViewModel.cs
@@ -4,6 +4,5 @@ namespace Chip8Emulator.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
 }
