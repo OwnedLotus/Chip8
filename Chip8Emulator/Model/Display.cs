16d7ba02e9e93f8c4bea0b92681af064a62cf02a
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-11-28 16:42:34 -0500

    display dim

--- Chip8Emulator/Model/Display.cs
+++ Chip8Emulator/Model/Display.cs
@@ -2,5 +2,12 @@ namespace Chip8Emulator.Model;
 
 public class Display
 {
+    byte[,] _display = new byte[64, 32];
+
+    public Display()
+    {
+        
+    }
     
 }
+// Sprite sizes are 8x15 maximum
