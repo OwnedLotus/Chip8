074e787e9119cedd1d32040485e97c45408f1d1e
Jonah Ringdahl <JonahRingdahl@outlook.com>
2024-11-28 16:12:40 -0500

    first commit

---
+++ Chip8Emulator/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Chip8Emulator.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Chip8Emulator/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Chip8Emulator.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
